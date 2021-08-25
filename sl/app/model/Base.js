7b53d9989982bab58c272f7484ad48e4bd87bbea
Stepan Ignatov <stepan.ignatov@pkcc-ps.ru>
2021-08-25 17:24:11 +0300

    #main Removed useless template components

--- sl/app/model/Base.js
+++
@@ -1,7 +0,0 @@
-Ext.define('SL.model.Base', {
-    extend: 'Ext.data.Model',
-
-    schema: {
-        namespace: 'SL.model'
-    }
-});
--- sl/app/model/Personnel.js
+++
@@ -1,7 +0,0 @@
-Ext.define('SL.model.Personnel', {
-    extend: 'SL.model.Base',
-
-    fields: [
-        'name', 'email', 'phone'
-    ]
-});
